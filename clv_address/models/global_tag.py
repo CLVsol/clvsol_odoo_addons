d22d5734eee1a75baa446884b23d8c2b22a29ba9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-03-15 13:51:02 -0300

    [IMP] clv_address

--- clv_address/models/global_tag.py
+++ clv_address/models/global_tag.py
@@ -18,20 +18,7 @@
 #
 ###############################################################################
 
-from odoo import models, fields
-
-
-class Address(models.Model):
-    _inherit = 'clv.address'
-
-    global_tag_ids = fields.Many2many(
-        comodel_name='clv.global_tag',
-        relation='clv_address_global_tag_rel',
-        column1='address_id',
-        column2='global_tag_id',
-        string='Global Tags'
-    )
-    global_tag_names = fields.Char(string='Global Tags', related='global_tag_ids.name', store=True)
+from odoo import api, models, fields
 
 
 class Tag(models.Model):
@@ -44,3 +31,44 @@ class Tag(models.Model):
         column2='global_tag_id',
         string='Addresses'
     )
+
+
+class Address(models.Model):
+    _inherit = 'clv.address'
+
+    global_tag_ids = fields.Many2many(
+        comodel_name='clv.global_tag',
+        relation='clv_address_global_tag_rel',
+        column1='address_id',
+        column2='global_tag_id',
+        string='Global Tags'
+    )
+    global_tag_names = fields.Char(
+        string='Global Tag Names',
+        compute='_compute_global_tag_names',
+        store=True
+    )
+    global_tag_names_suport = fields.Char(
+        string='Global Tag Names Suport',
+        compute='_compute_global_tag_names_suport',
+        store=False
+    )
+
+    @api.depends('global_tag_ids')
+    def _compute_global_tag_names(self):
+        for r in self:
+            r.global_tag_names = r.global_tag_names_suport
+
+    @api.multi
+    def _compute_global_tag_names_suport(self):
+        for r in self:
+            global_tag_names = False
+            for global_tag in r.global_tag_ids:
+                if global_tag_names is False:
+                    global_tag_names = global_tag.complete_name
+                else:
+                    global_tag_names = global_tag_names + ', ' + global_tag.complete_name
+            r.global_tag_names_suport = global_tag_names
+            if r.global_tag_names != global_tag_names:
+                record = self.env['clv.address'].search([('id', '=', r.id)])
+                record.write({'global_tag_ids': r.global_tag_ids})
