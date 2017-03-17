1387cdb1a55365101f7470013386a8d2683a8163
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-03-17 18:20:55 -0300

    [IMP] clv_person

--- clv_person/models/__init__.py
+++ clv_person/models/__init__.py
@@ -19,7 +19,6 @@
 ###############################################################################
 
 from . import person
-from . import person_code
 from . import person_category
 from . import person_log
 from . import address
