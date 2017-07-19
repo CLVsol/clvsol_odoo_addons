b6d4f389966b7c3972f2a5d3151814b6b1451584
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-07-19 16:14:06 +0000

    [ADD] clv_employee_history

--- clv_employee_history/models/__init__.py
+++ clv_employee_history/models/__init__.py
@@ -18,36 +18,7 @@
 #
 ###############################################################################
 
-{
-    'name': 'Employee',
-    'summary': 'Employee Module used by CLVsol Solutions.',
-    'version': '3.0.0',
-    'author': 'Carlos Eduardo Vercelino - CLVsol',
-    'category': 'Generic Modules/Others',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/CLVsol',
-    'images': [],
-    'depends': [
-        'hr',
-        'clv_base',
-        'clv_global_tag',
-    ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/hr_employee_view.xml',
-        'views/global_tag_view.xml',
-        'views/hr_employee_log_view.xml',
-        'views/hr_job_view.xml',
-        'views/hr_employee_menu_view.xml',
-        'wizard/hr_employee_updt_view.xml',
-    ],
-    'demo': [],
-    'test': [],
-    'init_xml': [],
-    'test': [],
-    'update_xml': [],
-    'installable': True,
-    'application': False,
-    'active': False,
-    'css': [],
-}
+from . import hr_employee_job_history
+from . import hr_employee_department_history
+from . import hr_employee_history
+from . import global_marker
--- clv_employee_history/wizard/__init__.py
+++ clv_employee_history/wizard/__init__.py
@@ -18,36 +18,5 @@
 #
 ###############################################################################
 
-{
-    'name': 'Employee',
-    'summary': 'Employee Module used by CLVsol Solutions.',
-    'version': '3.0.0',
-    'author': 'Carlos Eduardo Vercelino - CLVsol',
-    'category': 'Generic Modules/Others',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/CLVsol',
-    'images': [],
-    'depends': [
-        'hr',
-        'clv_base',
-        'clv_global_tag',
-    ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/hr_employee_view.xml',
-        'views/global_tag_view.xml',
-        'views/hr_employee_log_view.xml',
-        'views/hr_job_view.xml',
-        'views/hr_employee_menu_view.xml',
-        'wizard/hr_employee_updt_view.xml',
-    ],
-    'demo': [],
-    'test': [],
-    'init_xml': [],
-    'test': [],
-    'update_xml': [],
-    'installable': True,
-    'application': False,
-    'active': False,
-    'css': [],
-}
+import hr_employee_updt
+import hr_employee_history_updt
