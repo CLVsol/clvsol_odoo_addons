40bd0ba186b537588f1fc26d8b3e80d048703060
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-11-16 17:59:49 +0000

    [IMP] clv_person_mng (IMP wizard/person_mng_person_update.py)

--- clv_person_mng/wizard/person_mng_person_update.py
+++ clv_person_mng/wizard/person_mng_person_update.py
@@ -33,7 +33,7 @@ class PersonMngPersonUpdate(models.TransientModel):
     person_mng_ids = fields.Many2many(
         comodel_name='clv.person.mng',
         relation='clv_person_mng_person_update_rel',
-        string='Persons (Management)',
+        string='Persons (Mng)',
         default=_default_person_mng_ids
     )
 
