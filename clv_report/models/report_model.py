89224b086edfd27d6fe04a447065da41d30125ea
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-12-31 18:14:57 -0200

    [ADD] clv_report

---
+++ clv_report/models/report_model.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from datetime import *
+
+from odoo import fields, models
+
+
+class ObjectReport(models.AbstractModel):
+    _name = 'clv.object.report'
+
+    name = fields.Char(string='Name', index=True, required=True)
+
+    date_report = fields.Datetime(
+        string="Report Date", required=True, readonly=True,
+        default=lambda *a: datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    )
+
+    notes = fields.Text(string='Notes')
+
+    active = fields.Boolean(string='Active', default=True)
