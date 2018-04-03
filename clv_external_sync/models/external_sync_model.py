fd4ba30988e28e1157b3ddc8f160fcd0bb116039
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-04-03 15:05:50 -0300

    [IMP] clv_external_sync (ADD external_sync_host_login)

--- clv_external_sync/models/external_sync_model.py
+++ clv_external_sync/models/external_sync_model.py
@@ -18,7 +18,12 @@
 #
 ###############################################################################
 
-from odoo import models, fields
+import logging
+import xmlrpclib
+
+from odoo import api, models, fields
+
+_logger = logging.getLogger(__name__)
 
 
 class ExternalSyncModel(models.AbstractModel):
@@ -32,3 +37,92 @@ class ExternalSyncModel(models.AbstractModel):
          ('synchronized', 'Synchronized'),
          ], 'External Synchronization'
     )
+
+    @api.model
+    def external_sync_host_login(self, external_host, external_dbname, external_user, external_user_pw):
+
+        uid = False
+        sock = False
+
+        xmlrpc_sock_common_url = external_host + '/xmlrpc/common'
+        xmlrpc_sock_str = external_host + '/xmlrpc/object'
+
+        server_version = False
+
+        login_msg = ''
+        user_name = ''
+        company_name = ''
+
+        sock_common = xmlrpclib.ServerProxy(xmlrpc_sock_common_url)
+        try:
+            server_version = sock_common.version()
+        except Exception:
+            pass
+
+        if server_version is not False:
+            pass
+        else:
+            login_msg = '[11] Server is not responding.'
+            _logger.info(u'%s %s %s %s', '>>>>>',
+                         login_msg,
+                         user_name,
+                         company_name)
+            return uid, sock, login_msg
+
+        try:
+            sock_common = xmlrpclib.ServerProxy(xmlrpc_sock_common_url)
+            uid = sock_common.login(external_dbname, external_user, external_user_pw)
+            sock = xmlrpclib.ServerProxy(xmlrpc_sock_str)
+        except Exception:
+            pass
+
+        if uid is not False:
+            pass
+        else:
+            login_msg = '[21] Invalid Login/Pasword.'
+            _logger.info(u'%s %s %s %s', '>>>>>',
+                         login_msg,
+                         user_name,
+                         company_name)
+            return uid, sock, login_msg
+
+        try:
+            sock_common = xmlrpclib.ServerProxy(xmlrpc_sock_common_url)
+            uid = sock_common.login(external_dbname, external_user, external_user_pw)
+            sock = xmlrpclib.ServerProxy(xmlrpc_sock_str)
+        except Exception:
+            pass
+
+        user_fields = ['name', 'parent_id', ]
+        user_data = sock.execute(external_dbname, uid, external_user_pw, 'res.users', 'read',
+                                 uid, user_fields)
+        user_name = user_data['name']
+        parent_id = user_data['parent_id']
+
+        if parent_id is not False:
+            args = [('id', '=', parent_id[0])]
+            company_id = sock.execute(external_dbname, uid, external_user_pw, 'res.company', 'search', args)
+
+            company_fields = ['name', ]
+            company_data = sock.execute(external_dbname, uid, external_user_pw, 'res.company', 'read',
+                                        company_id[0], company_fields)
+            company_name = company_data['name']
+
+        if uid is not False:
+            login_msg = '[01] Login Ok.'
+            _logger.info(u'%s %s %s %s', '>>>>>',
+                         login_msg,
+                         user_name,
+                         company_name)
+
+        if uid is not False:
+
+            user_fields = ['name', 'parent_id', ]
+            user_data = sock.execute(external_dbname, uid, external_user_pw, 'res.users', 'read',
+                                     uid, user_fields)
+            user_name = user_data['name']
+            parent_id = user_data['parent_id'][0]
+
+            _logger.info(u'%s %s', '>>>>>>>>>>', user_data)
+
+        return uid, sock, login_msg
