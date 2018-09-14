85ba2e127502a570ad69745b4f22ae76ad1cf7b4
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-14 10:37:25 -0300

    [ADD] clv_lab_test

---
+++ clv_lab_test/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Lab Test',
+    'summary': 'Lab Test Module used by CLVsol Solutions.',
+    'version': '4.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'images': [],
+    'depends': [
+        'clv_base',
+    ],
+    'data': [
+        'security/lab_test_security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
