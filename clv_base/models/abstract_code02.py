d727924479a89cad861d4d482b2a8197f0e500e9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-20 20:45:52 -0300

    [IMP] clv_base (IMP models/abstract_code.py and models/abstract_code02.py)

--- clv_base/models/abstract_code02.py
+++ clv_base/models/abstract_code02.py
@@ -1,28 +1,12 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
 
 
 def format_code(code_seq, code_len):
-    code = map(int, str(code_seq))
+    code = list(map(int, str(code_seq)))
     while len(code) < 10:
         code.insert(0, 0)
     while len(code) < 12:
