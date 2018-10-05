d856137f0c4b79b23d8abec042ee6f21ca319c9b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-10-05 15:28:25 -0300

    [IMP] clv_external_sync (ADD wizard/external_sync_template_mass_edit)

--- clv_external_sync/wizard/__init__.py
+++ clv_external_sync/wizard/__init__.py
@@ -3,4 +3,5 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import external_sync_template_mass_edit
 from . import external_sync_schedule_exec
