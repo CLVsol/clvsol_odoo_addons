e1088e070813da64a13630eb8ee1f602e90a9f27
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-10-18 17:38:25 -0300

    [IMP] clv_lab_test (ADD wizard/lab_test_request_mass_edit)

--- clv_lab_test/wizard/__init__.py
+++ clv_lab_test/wizard/__init__.py
@@ -2,4 +2,5 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import lab_test_request_mass_edit
 from . import lab_test_type_duplicate
