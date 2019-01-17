c068e52750f083ddc801a1690bfb63a78c40016f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-01-17 16:29:17 -0200

    [REM] clv_animal

--- clv_animal/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import animal
-from . import animal_category
-from . import animal_category_log
-from . import res_partner
-from . import global_tag
