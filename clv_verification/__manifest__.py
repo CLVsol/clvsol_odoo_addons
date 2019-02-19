9c13ed9d4bf8c7060fda23905268bcc5d4ef6d90
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-02-19 20:16:57 -0300

    [ADD] clv_verification

---
+++ clv_verification/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Verification',
+    'summary': 'Verification Module used by CLVsol Solutions.',
+    'version': '4.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'images': [],
+    'depends': [
+        'clv_base',
+        # 'clv_global_log',
+    ],
+    'data': [
+        'security/verification_security.xml',
+        'security/ir.model.access.csv',
+        'views/verification_view.xml',
+        # 'views/verification_template_view.xml',
+        # 'views/verification_template_log_view.xml',
+        # 'views/verification_schedule_view.xml',
+        # 'views/verification_schedule_log_view.xml',
+        # 'views/verification_batch_view.xml',
+        # 'views/verification_batch_log_view.xml',
+        # 'views/verification_batch_member_view.xml',
+        # 'views/referenceable_model_view.xml',
+        # 'data/verification_batch_member.xml',
+        # 'wizard/verification_mass_edit_view.xml',
+        # 'wizard/verification_template_mass_edit_view.xml',
+        # 'wizard/verification_schedule_mass_edit_view.xml',
+        # 'wizard/verification_schedule_exec_view.xml',
+        # 'wizard/verification_batch_exec_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
---
+++ clv_verification/models/abstract_verification.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+from functools import reduce
+
+from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
+
+
+def secondsToStr(t):
+
+    return "%d:%02d:%02d.%03d" % reduce(lambda ll, b: divmod(ll[0], b) + ll[1:], [(t * 1000,), 1000, 60, 60])
+
+
+class AbstractVerification(models.AbstractModel):
+    _description = 'Abstract Verification'
+    _name = 'clv.abstract.verification'
+
+    date_last_verification = fields.Datetime(string="Last Verification")
+    verification = fields.Selection(
+        [('unknown', 'Unknown'),
+         ('failed', 'Failed'),
+         ('ok', 'Ok'),
+         ('missing', 'Missing'),
+         ], string='Verification', default='unknown'
+    )
+
+    def _object_verification(self, schedule, model_name):
+
+        from time import time
+        start = time()
+
+        _logger.info(u'%s %s', '>>>>>>>>>> Execution time: ', secondsToStr(time() - start))
+
+        schedule.verification_log +=  \
+            'Execution time: ' + str(secondsToStr(time() - start)) + '\n'
