411286d20a7e753c2a514b5b4c41f1455ec29318
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-02-21 20:17:38 -0300

    [IMP] clv_verification (IMP verification)

--- clv_verification/models/verification.py
+++ clv_verification/models/verification.py
@@ -10,34 +10,33 @@ class Verification(models.Model):
     _name = 'clv.verification'
     _inherit = 'clv.abstract.verification'
     _order = "id desc"
-    _rec_name = 'verification_reference_name'
+    _rec_name = 'reference_name'
 
-    verification_model = fields.Char(string='Model Name ', required=True)
-    verification_res_id = fields.Integer(
+    model = fields.Char(string='Model Name ', required=True)
+    res_id = fields.Integer(
         string='Record ID',
         help="ID of the target record in the database"
     )
-    verification_reference = fields.Char(
+    reference = fields.Char(
         string='Reference ',
-        compute='_compute_verification_reference',
+        compute='_compute_reference',
         readonly=True,
         store=True
     )
-    verification_reference_name = fields.Char(
+    reference_name = fields.Char(
         string='Reference Name',
-        compute='_compute_verification_reference',
+        compute='_compute_reference',
         readonly=True,
         store=True
     )
 
-    @api.depends('verification_model', 'verification_res_id')
-    def _compute_verification_reference(self):
+    @api.depends('model', 'res_id')
+    def _compute_reference(self):
         for record in self:
-            if (record.verification_model is not False) and (record.verification_res_id != 0):
-                record.verification_reference = "%s,%s" % (record.verification_model,
-                                                           record.verification_res_id)
-                Model = self.env[record.verification_model]
+            if (record.model is not False) and (record.res_id != 0):
+                record.reference = "%s,%s" % (record.model, record.res_id)
+                Model = self.env[record.model]
                 rec = Model.search([
-                    ('id', '=', record.verification_res_id),
+                    ('id', '=', record.res_id),
                 ])
-                record.verification_reference_name = rec.name_get()[0][1]
+                record.reference_name = rec.name_get()[0][1]
