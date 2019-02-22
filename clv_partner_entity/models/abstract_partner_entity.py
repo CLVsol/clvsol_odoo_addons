ead153c1dfc0fdda1884015a924a5943b5d46230
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-02-22 18:46:44 -0300

    [IMP] clv_partner_entity (IMP views/abstract_partner_entity_view.xml)

--- clv_partner_entity/models/abstract_partner_entity.py
+++ clv_partner_entity/models/abstract_partner_entity.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 LasLabs Inc.
-# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import threading
 import base64
