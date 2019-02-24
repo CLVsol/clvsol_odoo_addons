23e333dd3aa739987818af9ba07a0e7c1b4e7cdc
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-02-23 21:05:36 -0300

    [IMP] clv_address (IMP models/address_name.py)

--- clv_address/models/address_name.py
+++ clv_address/models/address_name.py
@@ -22,32 +22,31 @@ class Address(models.Model):
     @api.depends('street', 'street2')
     def _get_suggested_name(self):
         for record in self:
-            pass
-            # if record.street:
-            #     record.suggested_name = record.street
-            #     if record.street2:
-            #         record.suggested_name = record.suggested_name + ' - ' + record.street2
-            # elif record.name:
-            #     record.suggested_name = record.name
+            if record.street:
+                record.suggested_name = record.street
+                if record.street2:
+                    record.suggested_name = record.suggested_name + ' - ' + record.street2
+            elif record.name:
+                record.suggested_name = record.name
+            else:
+                record.suggested_name = 'x'
             # else:
-            #     record.suggested_name = 'x'
-            # # else:
-            # #     if not record.suggested_name:
-            # #         if record.code:
-            # #             record.suggested_name = record.code
+            #     if not record.suggested_name:
+            #         if record.code:
+            #             record.suggested_name = record.code
 
-    # @api.multi
-    # def write(self, values):
-    #     ret = super(Address, self).write(values)
-    #     for record in self:
-    #         if record.suggested_name is not False:
-    #             if record.automatic_set_name:
-    #                 if record.name != record.suggested_name:
-    #                     values['name'] = record.suggested_name
-    #                     super(Address, record).write(values)
-    #             else:
-    #                 if ('name' in values and values['name'] == '/') or \
-    #                    (record.name == '/'):
-    #                     values['name'] = record.suggested_name
-    #                     super(Address, record).write(values)
-    #     return ret
+    @api.multi
+    def write(self, values):
+        ret = super(Address, self).write(values)
+        for record in self:
+            if record.suggested_name is not False:
+                if record.automatic_set_name:
+                    if record.name != record.suggested_name:
+                        values['name'] = record.suggested_name
+                        super(Address, record).write(values)
+                else:
+                    if ('name' in values and values['name'] == '/') or \
+                       (record.name == '/'):
+                        values['name'] = record.suggested_name
+                        super(Address, record).write(values)
+        return ret
