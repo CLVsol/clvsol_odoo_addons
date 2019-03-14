93bcfffaf8465d3a6a086e8e5b42812534dda008
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-03-14 12:19:49 -0300

    [IMP] clv_person (IMP address)

--- clv_person/models/address.py
+++ clv_person/models/address.py
@@ -2,7 +2,11 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class Address(models.Model):
@@ -35,8 +39,43 @@ class Person(models.Model):
     ref_address_mobile_phone = fields.Char(string='Address Mobile', related='ref_address_id.mobile')
     ref_address_email = fields.Char(string='Address Email', related='ref_address_id.email')
 
-    ref_address_category_ids = fields.Char(
-        string='Address Categories',
+    ref_address_category_names = fields.Char(
+        string='Address Category Names',
         related='ref_address_id.category_ids.name',
         store=True
     )
+    ref_address_category_ids = fields.Many2many(
+        comodel_name='clv.address.category',
+        string='Address Categories',
+        related='ref_address_id.category_ids'
+    )
+
+    @api.multi
+    def do_person_get_ref_address_data(self):
+
+        for person in self:
+
+            _logger.info(u'>>>>> %s', person.ref_address_id)
+
+            if (person.ref_address_id.id is not False):
+
+                data_values = {}
+
+                if person.ref_address_id.id is not False:
+
+                    data_values['ref_address_id'] = person.ref_address_id.id
+
+                    data_values['street'] = person.ref_address_id.street
+                    data_values['street2'] = person.ref_address_id.street2
+                    data_values['zip'] = person.ref_address_id.zip
+                    data_values['city'] = person.ref_address_id.city
+                    data_values['state_id'] = person.ref_address_id.state_id.id
+                    data_values['country_id'] = person.ref_address_id.country_id.id
+                    # data_values['phone'] = person.ref_address_id.phone
+                    # data_values['mobile'] = person.ref_address_id.mobile
+
+                _logger.info(u'>>>>>>>>>> %s', data_values)
+
+                person.write(data_values)
+
+        return True
