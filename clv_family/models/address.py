1121e60fcd6372ac81e6b47d9fd13d537a91a3a5
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-03-14 12:20:37 -0300

    [IMP] clv_family (IMP address)

--- clv_family/models/address.py
+++ clv_family/models/address.py
@@ -2,7 +2,11 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class Address(models.Model):
@@ -35,8 +39,43 @@ class Family(models.Model):
     ref_address_mobile_phone = fields.Char(string='Address Mobile', related='ref_address_id.mobile')
     ref_address_email = fields.Char(string='Address Email', related='ref_address_id.email')
 
-    ref_address_category_ids = fields.Char(
-        string='Address Categories',
+    ref_address_category_names = fields.Char(
+        string='Address Category Names',
         related='ref_address_id.category_ids.name',
         store=True
     )
+    ref_address_category_ids = fields.Many2many(
+        comodel_name='clv.address.category',
+        string='Address Categories',
+        related='ref_address_id.category_ids'
+    )
+
+    @api.multi
+    def do_family_get_ref_address_data(self):
+
+        for family in self:
+
+            _logger.info(u'>>>>> %s', family.ref_address_id)
+
+            if (family.ref_address_id.id is not False):
+
+                data_values = {}
+
+                if family.ref_address_id.id is not False:
+
+                    data_values['ref_address_id'] = family.ref_address_id.id
+
+                    data_values['street'] = family.ref_address_id.street
+                    data_values['street2'] = family.ref_address_id.street2
+                    data_values['zip'] = family.ref_address_id.zip
+                    data_values['city'] = family.ref_address_id.city
+                    data_values['state_id'] = family.ref_address_id.state_id.id
+                    data_values['country_id'] = family.ref_address_id.country_id.id
+                    # data_values['phone'] = family.ref_address_id.phone
+                    # data_values['mobile'] = family.ref_address_id.mobile
+
+                _logger.info(u'>>>>>>>>>> %s', data_values)
+
+                family.write(data_values)
+
+        return True
