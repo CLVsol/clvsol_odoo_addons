2ebaeda9a1900eeb6a4e7fec4f124fb609bb4e53
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-02 14:50:23 -0300

    [IMP] clv_mfile

--- clv_mfile/__init__.py
+++ clv_mfile/__init__.py
@@ -1,4 +1,5 @@
-# © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
