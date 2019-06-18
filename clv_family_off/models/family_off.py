b01a2658cf1b7f96376cd5d267e3cde1a1eba9d5
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-06-18 20:45:06 -0300

    [IMP] clv_family_off (IMP models/family_off.pymodels/res_partner.py)

--- clv_family_off/models/family_off.py
+++ clv_family_off/models/family_off.py
@@ -21,6 +21,9 @@ class FamilyOff(models.Model):
         vals.update({
             'customer': True,
         })
+        vals.update({
+            'type': self._name,
+        })
         return vals
 
     @api.model_cr_context
