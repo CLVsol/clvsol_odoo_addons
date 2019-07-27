d6a4df3fabeb6cacb2373fc8387123f49a35ce38
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-26 21:43:40 -0300

    [IMP] clv_person_off (IMP family_off)

--- clv_person_off/models/family_off.py
+++ clv_person_off/models/family_off.py
@@ -2,7 +2,11 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class FamilyOff(models.Model):
@@ -36,3 +40,25 @@ class PersonOff(models.Model):
         related='family_off_id.category_ids.name',
         store=True
     )
+
+    @api.multi
+    def do_person_off_remove_family_off(self):
+
+        for person_off in self:
+
+            _logger.info(u'>>>>> %s', person_off.family_off_id)
+
+            if (person_off.reg_state in ['draft', 'revised']) and \
+               (person_off.family_off_id.id is not False):
+
+                data_values = {}
+
+                if person_off.family_off_id.id is not False:
+
+                    data_values['family_off_id'] = False
+
+                _logger.info(u'>>>>>>>>>> %s', data_values)
+
+                person_off.write(data_values)
+
+        return True
