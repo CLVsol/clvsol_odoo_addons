46fb9e93495bbea7ae5476cf8f429745298616b6
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-27 14:49:11 -0300

    [REF] clv_family_off

--- clv_family_off/models/family_off.py
+++ clv_family_off/models/family_off.py
@@ -3,8 +3,12 @@
 # Copyright 2016 LasLabs Inc.
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
+import logging
+
 from odoo import api, fields, models
 from odoo.modules import get_module_resource
+
+_logger = logging.getLogger(__name__)
 
 
 class FamilyOff(models.Model):
@@ -37,3 +41,27 @@ class FamilyOff(models.Model):
             'clv_family_off', 'static/src/img', 'family_off-avatar.png',
         )
         return image_path
+
+    @api.multi
+    def do_family_off_clear_address_data(self):
+
+        for family_off in self:
+
+            # _logger.info(u'>>>>> %s', family_off.ref_address_id)
+
+            # if (family_off.reg_state in ['draft', 'revised']):
+
+            data_values = {}
+
+            data_values['street'] = False
+            data_values['street2'] = False
+            data_values['zip'] = False
+            data_values['city'] = False
+            data_values['state_id'] = False
+            data_values['country_id'] = False
+            # data_values['phone'] = False
+            # data_values['mobile'] = False
+
+            _logger.info(u'>>>>>>>>>> %s', data_values)
+
+            family_off.write(data_values)
