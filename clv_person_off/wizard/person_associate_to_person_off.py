d6595088b5ab153804d3f7a10de94f729f435b21
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-29 16:09:32 -0300

    [IMP] clv_person_off (IMP wizard/person_associate_to_person_off)

--- clv_person_off/wizard/person_associate_to_person_off.py
+++ clv_person_off/wizard/person_associate_to_person_off.py
@@ -28,6 +28,18 @@ class PersonAssociateToPersonOff(models.TransientModel):
         readonly=True
     )
 
+    create_new_address_off = fields.Boolean(
+        string='Create new Address (Off)',
+        default=True,
+        readonly=True
+    )
+
+    create_new_family_off = fields.Boolean(
+        string='Create new Family (Off)',
+        default=True,
+        readonly=True
+    )
+
     @api.multi
     def _reopen_form(self):
         self.ensure_one()
@@ -79,6 +91,109 @@ class PersonAssociateToPersonOff(models.TransientModel):
                     new_person_off.write(values)
 
                     new_person_off.do_person_off_get_related_person_data()
+
+            if person.ref_address_id.id is not False:
+
+                AddressOff = self.env['clv.address_off']
+                address_off = AddressOff.search([
+                    ('related_address_id', '=', person.ref_address_id.id),
+                ])
+                _logger.info(u'%s %s %s', '>>>>>>>>>>', 'address_off_id:', address_off.id)
+
+                if address_off.id is not False:
+
+                    new_address_off = address_off
+
+                else:
+
+                    if self.create_new_address_off:
+
+                        values = {}
+                        values['street'] = new_person_off.ref_address_id.street
+
+                        _logger.info(u'%s %s %s', '>>>>>>>>>>', 'values:', values)
+                        new_address_off = AddressOff.create(values)
+                        _logger.info(u'%s %s %s', '>>>>>>>>>>', 'new_address_off:', new_address_off)
+
+                        values = {}
+                        values['related_address_id'] = new_person_off.ref_address_id.id
+                        _logger.info(u'%s %s %s', '>>>>>>>>>>', 'values:', values)
+                        new_address_off.write(values)
+
+                        new_address_off.do_address_off_get_related_address_data()
+
+                data_values = {}
+                data_values['ref_address_off_id'] = new_address_off.id
+                _logger.info(u'>>>>>>>>>> %s', data_values)
+                new_person_off.write(data_values)
+
+            if person.family_id.id is not False:
+
+                FamilyOff = self.env['clv.family_off']
+                family_off = FamilyOff.search([
+                    ('related_family_id', '=', person.family_id.id),
+                ])
+                _logger.info(u'%s %s %s', '>>>>>>>>>>', 'family_off_id:', family_off.id)
+
+                if family_off.id is not False:
+
+                    new_family_off = family_off
+
+                else:
+
+                    if self.create_new_family_off:
+
+                        values = {}
+                        values['street'] = new_person_off.family_id.street
+
+                        _logger.info(u'%s %s %s', '>>>>>>>>>>', 'values:', values)
+                        new_family_off = FamilyOff.create(values)
+                        _logger.info(u'%s %s %s', '>>>>>>>>>>', 'new_family_off:', new_family_off)
+
+                        values = {}
+                        values['related_family_id'] = new_person_off.family_id.id
+                        _logger.info(u'%s %s %s', '>>>>>>>>>>', 'values:', values)
+                        new_family_off.write(values)
+
+                        new_family_off.do_family_off_get_related_family_data()
+
+                data_values = {}
+                data_values['family_off_id'] = new_family_off.id
+                _logger.info(u'>>>>>>>>>> %s', data_values)
+                new_person_off.write(data_values)
+
+                AddressOff = self.env['clv.address_off']
+                address_off = AddressOff.search([
+                    ('related_address_id', '=', new_family_off.ref_address_id.id),
+                ])
+                _logger.info(u'%s %s %s', '>>>>>>>>>>', 'address_off_id:', address_off.id)
+
+                if address_off.id is not False:
+
+                    new_address_off = address_off
+
+                else:
+
+                    if self.create_new_address_off:
+
+                        values = {}
+                        values['street'] = new_family_off.ref_address_id.street
+
+                        _logger.info(u'%s %s %s', '>>>>>>>>>>', 'values:', values)
+                        new_address_off = AddressOff.create(values)
+                        _logger.info(u'%s %s %s', '>>>>>>>>>>', 'new_address_off:', new_address_off)
+
+                        values = {}
+                        values['related_address_id'] = new_family_off.ref_address_id.id
+                        _logger.info(u'%s %s %s', '>>>>>>>>>>', 'values:', values)
+                        new_address_off.write(values)
+
+                        new_address_off.do_address_off_get_related_address_data()
+
+                data_values = {}
+                data_values['ref_address_off_id'] = new_address_off.id
+                _logger.info(u'>>>>>>>>>> %s', data_values)
+                new_family_off.write(data_values)
 
         if person_count == 1:
 
