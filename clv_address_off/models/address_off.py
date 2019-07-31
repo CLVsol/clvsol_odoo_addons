64e8458de7ca86e5e5bd2bb24c11a28423f2978c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-31 11:21:09 -0300

    [IMP] clv_address_off (IMP address_off)

--- clv_address_off/models/address_off.py
+++ clv_address_off/models/address_off.py
@@ -2,8 +2,12 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+
 from odoo import api, fields, models
 from odoo.modules import get_module_resource
+
+_logger = logging.getLogger(__name__)
 
 
 class AddressOff(models.Model):
@@ -36,3 +40,27 @@ class AddressOff(models.Model):
             'clv_address_off', 'static/src/img', 'address_off-avatar.png',
         )
         return image_path
+
+    @api.multi
+    def do_address_off_clear_address_data(self):
+
+        for address_off in self:
+
+            # _logger.info(u'>>>>> %s', address_off.ref_address_id)
+
+            # if (address_off.reg_state in ['draft', 'revised']):
+
+            data_values = {}
+
+            data_values['street'] = False
+            data_values['street2'] = False
+            data_values['zip'] = False
+            data_values['city'] = False
+            data_values['state_id'] = False
+            data_values['country_id'] = False
+            # data_values['phone'] = False
+            # data_values['mobile'] = False
+
+            _logger.info(u'>>>>>>>>>> %s', data_values)
+
+            address_off.write(data_values)
