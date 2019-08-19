c4ce7e920268a9a930792b243eb0f7a0e12d2d99
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-08-19 11:56:50 -0300

    [IMP] clv_address (IMP wizard/address_mass_edit)

--- clv_address/wizard/address_mass_edit.py
+++ clv_address/wizard/address_mass_edit.py
@@ -77,6 +77,12 @@ class AddressMassEdit(models.TransientModel):
          ], string='Markers:', default=False, readonly=False, required=False
     )
 
+    partner_entity_code_selection = fields.Selection(
+        [('set', 'Set'),
+         ('remove', 'Remove'),
+         ], string='Partner Entity Code:', default=False, readonly=False, required=False
+    )
+
     @api.multi
     def _reopen_form(self):
         self.ensure_one()
@@ -178,5 +184,16 @@ class AddressMassEdit(models.TransientModel):
                     m2m_list.append((4, marker_id.id))
                 _logger.info(u'%s %s', '>>>>>>>>>>', m2m_list)
                 address.marker_ids = m2m_list
+
+            if self.partner_entity_code_selection == 'set':
+                if address.entity_code != address.code:
+                    vals = {}
+                    vals['entity_code'] = address.code
+                    address.write(vals)
+            if self.partner_entity_code_selection == 'remove':
+                if address.entity_code is not False:
+                    vals = {}
+                    vals['entity_code'] = False
+                    address.write(vals)
 
         return True
