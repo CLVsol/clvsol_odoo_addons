9b6e58404343e96edc097c8b5bc633ef5a64f3e7
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-09-02 08:48:10 -0300

    [IMP] clv_document (IMP document_category)

--- clv_document/models/__init__.py
+++ clv_document/models/__init__.py
@@ -4,10 +4,10 @@
 
 from . import document
 from . import document_log
+from . import document_type
+from . import document_type_log
 from . import document_category
 from . import document_category_log
 from . import global_tag
-from . import document_type
-from . import document_type_log
 from . import document_item
 from . import referenceable_model
--- clv_document/models/document_category.py
+++ clv_document/models/document_category.py
@@ -33,6 +33,14 @@ class DocumentCategory(models.Model):
         string='Documents'
     )
 
+    document_type_ids = fields.Many2many(
+        comodel_name='clv.document.type',
+        relation='clv_document_type_category_rel',
+        column1='category_id',
+        column2='document_type_id',
+        string='Document Types'
+    )
+
     _sql_constraints = [
         ('code_uniq',
          'UNIQUE (code)',
@@ -78,4 +86,45 @@ class Document(models.Model):
             r.category_names_suport = category_names
             if r.category_names != category_names:
                 record = self.env['clv.document'].search([('id', '=', r.id)])
+                record.write({'category_ids': r.category_ids})
+
+
+class DocumentType(models.Model):
+    _inherit = "clv.document.type"
+
+    category_ids = fields.Many2many(
+        comodel_name='clv.document.category',
+        relation='clv_document_type_category_rel',
+        column1='document_type_id',
+        column2='category_id',
+        string='Categories'
+    )
+    category_names = fields.Char(
+        string='Category Names',
+        compute='_compute_category_names',
+        store=True
+    )
+    category_names_suport = fields.Char(
+        string='Category Names Suport',
+        compute='_compute_category_names_suport',
+        store=False
+    )
+
+    @api.depends('category_ids')
+    def _compute_category_names(self):
+        for r in self:
+            r.category_names = r.category_names_suport
+
+    @api.multi
+    def _compute_category_names_suport(self):
+        for r in self:
+            category_names = False
+            for category in r.category_ids:
+                if category_names is False:
+                    category_names = category.complete_name
+                else:
+                    category_names = category_names + ', ' + category.complete_name
+            r.category_names_suport = category_names
+            if r.category_names != category_names:
+                record = self.env['clv.document.type'].search([('id', '=', r.id)])
                 record.write({'category_ids': r.category_ids})
