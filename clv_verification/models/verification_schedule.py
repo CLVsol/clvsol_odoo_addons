e34312a77fb0ef141bd179175cacc638b5c51a16
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-09-22 11:47:33 -0300

    [IMP] clv_verification (IMP verification_schedule)

--- clv_verification/models/verification_schedule.py
+++ clv_verification/models/verification_schedule.py
@@ -46,11 +46,48 @@ class VerificationSchedule(models.Model):
         help="Name of the method to be called when the verification job is processed."
     )
 
+    verification_max_task = fields.Integer(
+        string='Max Task Registers'
+    )
+
+    verification_disable_identification = fields.Boolean(
+        string='Disable Identification'
+    )
+
+    verification_disable_check_missing = fields.Boolean(
+        string='Disable Check Missing'
+    )
+
+    verification_disable_inclusion = fields.Boolean(
+        string='Disable Inclusion'
+    )
+
+    verification_disable_verification = fields.Boolean(
+        string='Disable Verification'
+    )
+
+    verification_last_update_start = fields.Datetime(
+        string="Last Update (Start)"
+    )
+
+    verification_last_update_end = fields.Datetime(
+        string="Last Update (End)"
+    )
+
     verification_log = fields.Text(
         string="Verification Log"
     )
 
     active = fields.Boolean(string='Active', default=1)
+
+    date_last_verification = fields.Datetime(
+        string='Last Verification Date',
+        readonly=True
+    )
+    upmost_last_update = fields.Datetime(
+        string="Upmost Last Update",
+        readonly=True
+    )
 
     # _sql_constraints = [
     #     ('name_uniq',
@@ -64,6 +101,14 @@ class VerificationSchedule(models.Model):
         schedule = super().create(values)
 
         if schedule.template_id.id is not False:
+
+            schedule.verification_max_task = schedule.template_id.verification_max_task
+            schedule.verification_disable_identification = schedule.template_id.verification_disable_identification
+            schedule.verification_disable_check_missing = schedule.template_id.verification_disable_check_missing
+            schedule.verification_disable_inclusion = schedule.template_id.verification_disable_inclusion
+            schedule.verification_disable_verification = schedule.template_id.verification_disable_verification
+            schedule.verification_last_update_start = schedule.template_id.verification_last_update_start
+            schedule.verification_last_update_end = schedule.template_id.verification_last_update_end
             schedule.model = schedule.template_id.model
             schedule.method = schedule.template_id.method
 
@@ -72,8 +117,33 @@ class VerificationSchedule(models.Model):
     @api.onchange('template_id')
     def onchange_template_id(self):
         if self.template_id.id:
+
+            self.verification_max_task = self.template_id.verification_max_task
+            self.verification_disable_identification = self.template_id.verification_disable_identification
+            self.verification_disable_check_missing = self.template_id.verification_disable_check_missing
+            self.verification_disable_inclusion = self.template_id.verification_disable_inclusion
+            self.verification_disable_verification = self.template_id.verification_disable_verification
+            self.verification_last_update_start = self.template_id.verification_last_update_start
+            self.verification_last_update_end = self.template_id.verification_last_update_end
             self.model = self.template_id.model
             self.method = self.template_id.method
+
+    @api.model
+    def verification_last_update_args(self):
+
+        args = []
+        if self.verification_last_update_start is not False and \
+           self.verification_last_update_end is False:
+            args = [('write_date', '>=', self.verification_last_update_start), ]
+        if self.verification_last_update_start is False and \
+           self.verification_last_update_end is not False:
+            args += [('write_date', '<=', self.verification_last_update_end), ]
+        if self.verification_last_update_start is not False and \
+           self.verification_last_update_end is not False:
+            args += [('write_date', '>=', self.verification_last_update_start),
+                     ('write_date', '<=', self.verification_last_update_end), ]
+
+        return args
 
 
 class VerificationTemplate(models.Model):
