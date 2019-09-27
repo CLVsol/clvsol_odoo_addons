3565583201333bccc01bd4c4c53c6acefd2db0c6
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-09-27 14:34:03 -0300

    [IMP] clv_verification (IMP verification_outcome)

--- clv_verification/models/__init__.py
+++ clv_verification/models/__init__.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import abstract_verification_outcome
 from . import verification_outcome
 from . import verification_outcome_log
 from . import verification_template
