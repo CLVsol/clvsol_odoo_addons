89a343cf810582d1c946d891eed862da0c706ff1
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-05-06 14:21:37 -0300

    [IMP] clv_export (ADD wizard/model_export_refresh)

--- clv_export/wizard/__init__.py
+++ clv_export/wizard/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import model_export_refresh
 from . import model_export_execute
 from . import model_export_template_duplicate
 from . import model_export_template_field_copy
