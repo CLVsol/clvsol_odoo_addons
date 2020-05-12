23acf0c17d75f14dabed11ac76abc5a06b603649
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-05-12 12:56:47 -0300

    Migrated module clv_external_sync to repositoty clvsol_odoo_addons_sync

--- clv_external_sync/wizard/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import external_sync_mass_edit
-from . import external_sync_template_mass_edit
-from . import external_sync_schedule_mass_edit
-from . import external_sync_schedule_exec
-from . import external_sync_batch_exec
