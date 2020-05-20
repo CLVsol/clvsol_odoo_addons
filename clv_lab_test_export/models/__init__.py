84155e7e12df60f00fb889e276838176384d9998
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-05-20 15:47:11 -0300

    Migrated Export Modules to repository clv_odoo_addons_export

--- clv_lab_test_export/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import model_export_template_lab_test_criterion
-from . import model_export_lab_test_criterion
