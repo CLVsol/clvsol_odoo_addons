a7f819a56492a8f733259022c8eb4523d217b163
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-06-15 20:08:49 -0300

    [IMP] clv_address_aux (IMP address_aux_category)

--- clv_address_aux/models/address_aux_category.py
+++ clv_address_aux/models/address_aux_category.py
@@ -20,19 +20,33 @@ class AddressAux(models.Model):
         compute='_compute_category_names',
         store=True
     )
-    category_names_suport = fields.Char(
-        string='Category Names Suport',
-        compute='_compute_category_names_suport',
-        store=False
-    )
+    # category_names_suport = fields.Char(
+    #     string='Category Names Suport',
+    #     compute='_compute_category_names_suport',
+    #     store=False
+    # )
+
+    # @api.depends('category_ids')
+    # def _compute_category_names(self):
+    #     for r in self:
+    #         r.category_names = r.category_names_suport
+
+    # # @api.multi
+    # def _compute_category_names_suport(self):
+    #     for r in self:
+    #         category_names = False
+    #         for category in r.category_ids:
+    #             if category_names is False:
+    #                 category_names = category.name
+    #             else:
+    #                 category_names = category_names + ', ' + category.name
+    #         r.category_names_suport = category_names
+    #         if r.category_names != category_names:
+    #             record = self.env['clv.address_aux'].search([('id', '=', r.id)])
+    #             record.write({'category_ids': r.category_ids})
 
     @api.depends('category_ids')
     def _compute_category_names(self):
-        for r in self:
-            r.category_names = r.category_names_suport
-
-    # @api.multi
-    def _compute_category_names_suport(self):
         for r in self:
             category_names = False
             for category in r.category_ids:
@@ -40,7 +54,4 @@ class AddressAux(models.Model):
                     category_names = category.name
                 else:
                     category_names = category_names + ', ' + category.name
-            r.category_names_suport = category_names
-            if r.category_names != category_names:
-                record = self.env['clv.address_aux'].search([('id', '=', r.id)])
-                record.write({'category_ids': r.category_ids})
+            r.category_names = category_names
