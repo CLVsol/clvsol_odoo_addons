0684eceec668f1502e14823a8a3f6f54760c89d1
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-07-30 17:55:15 -0300

    [IMP] clv_survey (IMP models/survey_user_input.py)

--- clv_survey/models/survey_user_input.py
+++ clv_survey/models/survey_user_input.py
@@ -12,6 +12,8 @@ _logger = logging.getLogger(__name__)
 class SurveyUserInput(models.Model):
     _inherit = 'survey.user_input'
 
+    _rec_name = 'token'
+
     def get_value(self, question_code):
 
         SurveyQuestion = self.env['survey.question']
