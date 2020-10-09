587da5d066cbe4108933e7656854948abdd5677e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-09 11:58:26 -0300

    [ADD] clv_address_history (Migrated from repository "clvsol_odoo_addons_history")

---
+++ clv_address_history/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Address History',
+    'summary': 'Address History Module used in CLVsol Solutions.',
+    'version': '12.0.4.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_address',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/address_history_view.xml',
+        'views/phase_view.xml',
+        'views/address_view.xml',
+        'views/address_history_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
---
+++ clv_address_history/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import address_history
+from . import phase
+from . import address
+from . import address_category
+from . import address_marker
+from . import address_tag
