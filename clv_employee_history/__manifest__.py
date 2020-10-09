43c8fe6b33f4312c7d7e609f065214a88451c536
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-09 10:00:12 -0300

    [ADD] clv_employee_history (Migrated from repository "clvsol_odoo_addons_history")

---
+++ clv_employee_history/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Employee History',
+    'summary': 'Employee History Module used in CLVsol Solutions.',
+    'version': '12.0.4.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_employee',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_employee_history_view.xml',
+        'views/phase_view.xml',
+        'wizard/hr_employee_history_updt_view.xml',
+        'views/employee_history_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
