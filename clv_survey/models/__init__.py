d3f6906c89fe9e82910ec1faad784b11f3809e18
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-01-15 16:01:13 -0300

    [IMP] clv_survey (IMP models/__init__.py)

--- clv_survey/models/__init__.py
+++ clv_survey/models/__init__.py
@@ -6,5 +6,4 @@ from . import survey_survey
 from . import survey_question
 from . import survey_question_answer
 from . import survey_user_input
-from . import survey_user_input
 from . import phase
