59a593212bd96a208aebd991a3db786e0f252cf4
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-01-20 14:25:48 -0300

    [IMP] clv_survey (IMP models/survey_user_input.py)

--- clv_survey/models/survey_user_input.py
+++ clv_survey/models/survey_user_input.py
@@ -36,8 +36,8 @@ class SurveyUserInput(models.Model):
     def get_value(self, question_code):
 
         SurveyQuestion = self.env['survey.question']
-        SurveyLabel = self.env['survey.label']
-        SurveyUserInput = self.env['survey.user_input_line']
+        SurveyQuestionAnswer = self.env['survey.question.answer']
+        SurveyUserInput = self.env['survey.user_input.line']
 
         survey_question_search = SurveyQuestion.search([
             ('code', '=', question_code[:11]),
@@ -48,50 +48,53 @@ class SurveyUserInput(models.Model):
                 ('question_id', '=', survey_question_search.id),
             ])
         else:
-            survey_label_search = SurveyLabel.search([
+            survey_question_answer_search = SurveyQuestionAnswer.search([
                 ('code', '=', question_code),
             ])
             survey_user_input_line_search = SurveyUserInput.search([
                 ('user_input_id', '=', self.id),
                 ('question_id', '=', survey_question_search.id),
-                ('value_suggested_row', '=', survey_label_search.id),
+                ('suggested_answer_id', '=', survey_question_answer_search.id),
             ])
 
         value = False
 
-        if survey_question_search.question_type == 'textbox':
-            value = survey_user_input_line_search.value_text
+        if survey_question_search.question_type == 'char_box':
+            value = survey_user_input_line_search.value_char_box
+
+        if survey_question_search.question_type == 'date':
+            value = survey_user_input_line_search.value_date
 
         if survey_question_search.question_type == 'simple_choice':
             value = ''
             if question_code[11:13] == '_C':
                 for survey_user_input_line_reg in survey_user_input_line_search:
-                    if survey_user_input_line_reg.value_text is not False:
-                        value = survey_user_input_line_reg.value_text
+                    if survey_user_input_line_reg.value_char_box is not False:
+                        value = survey_user_input_line_reg.value_char_box
             else:
                 for survey_user_input_line_reg in survey_user_input_line_search:
                     if value == '':
-                        value = survey_user_input_line_reg.value_suggested.value
+                        value = survey_user_input_line_reg.suggested_answer_id.value
                     else:
-                        if survey_user_input_line_reg.value_suggested.value is not False:
-                            value = value + '; ' + survey_user_input_line_reg.value_suggested.value
+                        if survey_user_input_line_reg.suggested_answer_id.value is not False:
+                            value = value + '; ' + survey_user_input_line_reg.suggested_answer_id.value
                         # else:
                         #     value = value + '; False'
-                # value = survey_user_input_line_search.value_suggested.value
+                # value = survey_user_input_line_search.suggested_answer_id.value
 
         if survey_question_search.question_type == 'multiple_choice':
             value = ''
             if question_code[11:13] == '_C':
                 for survey_user_input_line_reg in survey_user_input_line_search:
-                    if survey_user_input_line_reg.value_text is not False:
-                        value = survey_user_input_line_reg.value_text
+                    if survey_user_input_line_reg.value_char_box is not False:
+                        value = survey_user_input_line_reg.value_char_box
             else:
                 for survey_user_input_line_reg in survey_user_input_line_search:
                     if value == '':
-                        value = survey_user_input_line_reg.value_suggested.value
+                        value = survey_user_input_line_reg.suggested_answer_id.value
                     else:
-                        if survey_user_input_line_reg.value_suggested.value is not False:
-                            value = value + '; ' + survey_user_input_line_reg.value_suggested.value
+                        if survey_user_input_line_reg.suggested_answer_id.value is not False:
+                            value = value + '; ' + survey_user_input_line_reg.suggested_answer_id.value
                         # else:
                         #     value = value + '; False'
 
@@ -99,10 +102,10 @@ class SurveyUserInput(models.Model):
             value = ''
             for survey_user_input_line_reg in survey_user_input_line_search:
                 if value == '':
-                    value = survey_user_input_line_reg.value_suggested.value
+                    value = survey_user_input_line_reg.suggested_answer_id.value
                 else:
-                    if survey_user_input_line_reg.value_suggested.value is not False:
-                        value = value + '; ' + survey_user_input_line_reg.value_suggested.value
+                    if survey_user_input_line_reg.suggested_answer_id.value is not False:
+                        value = value + '; ' + survey_user_input_line_reg.suggested_answer_id.value
 
         return value
 
