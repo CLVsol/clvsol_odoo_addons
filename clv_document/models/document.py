37dded575d9c26a362d50b2fe4f6d0423421b377
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-08-13 10:46:38 -0300

    [IMP] clv_document (IMP document)

--- clv_document/models/document.py
+++ clv_document/models/document.py
@@ -4,7 +4,8 @@
 
 from datetime import datetime
 
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class Document(models.Model):
@@ -23,7 +24,36 @@ class Document(models.Model):
     )
     date_document = fields.Date(string='Document Date')
 
+    parent_id = fields.Many2one(
+        comodel_name='clv.document',
+        string='Parent Document',
+        index=True,
+        ondelete='restrict'
+    )
+
+    child_ids = fields.One2many(
+        comodel_name='clv.document',
+        inverse_name='parent_id',
+        string='Child Documents'
+    )
+    count_children = fields.Integer(
+        string='Documents (count)',
+        compute='_compute_count_children',
+        # store=True
+    )
+
+    # @api.depends('child_ids')
+    def _compute_count_children(self):
+        for r in self:
+            r.count_children = len(r.child_ids)
+
     active = fields.Boolean(string='Active', default=1)
+
+    @api.constrains('parent_id')
+    def _check_category_recursion(self):
+        if not self._check_recursion():
+            raise ValidationError(_('Error ! You cannot create recursive documnets.'))
+        return True
 
     _sql_constraints = [
         ('code_uniq',
