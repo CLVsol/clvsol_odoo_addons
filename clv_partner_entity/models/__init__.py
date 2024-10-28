d0d4325af779e0d96a4c076dab2b1e0cbe934748
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-10-28 20:23:24 -0300

    [REM] Initialize version 17.0

--- clv_partner_entity/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import res_partner
-from . import res_partner_log
-from . import abstract_partner_entity
-from . import abstract_partner_entity_log
