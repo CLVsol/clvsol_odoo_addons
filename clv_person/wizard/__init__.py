09ae07edd69da614828d9e2308be95bb05b734ab
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-10-30 15:17:16 -0300

    [REM] Initialize version 16.0]

--- clv_person/wizard/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import person_mass_edit
-from . import person_contact_information_updt
-from . import person_associate_to_family
-from . import person_address_mass_edit
--- clv_person_history/wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import person_history_updt
-from . import person_history_person_associate_to_set
-from . import person_history_person_mass_edit
