0b89a550c939f9a0a9a8342fea262a6ea83bfc73
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-11-17 14:18:51 -0300

    [ADD] clv_phase

---
+++ clv_phase/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Phase',
+    'summary': 'Phase Module used by CLVsol Solutions.',
+    'version': '16.0.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_base',
+    ],
+    'data': [
+        'security/phase_security.xml',
+        'security/ir.model.access.csv',
+        'views/phase_view.xml',
+        'views/global_settings_view.xml',
+        'views/phase_menu_view.xml',
+        'data/default_value.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
