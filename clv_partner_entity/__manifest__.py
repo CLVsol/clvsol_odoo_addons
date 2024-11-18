1bfbda648ab629f35daa54292fcad32136f5e9fa
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-11-17 22:12:19 -0300

    [ADD] clv_partner_entity

---
+++ clv_partner_entity/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Partner Entity',
+    'summary': 'Partner Entity Module used by CLVsol Solutions.',
+    'version': '16.0.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'contacts',
+        'base_address_extended',
+        'clv_base',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+        'views/base_address_city_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
---
+++ clv_partner_entity/models/res_partner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    entity_code = fields.Char(string='Entity Code', required=False)
