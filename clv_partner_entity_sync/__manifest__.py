6fd58583ae4254392ad4bc0567cbcc8512cd83fd
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-11-24 17:57:48 -0300

    [ADD] clv_partner_entity_sync

---
+++ clv_partner_entity_sync/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Partner Entity External Sync',
+    'summary': 'Partner Entity External Sync Module used by CLVsol Solutions.',
+    'version': '16.0.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'base',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/res_country_sync.xml',
+        'data/res_country_state_sync.xml',
+        'data/res_city_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
