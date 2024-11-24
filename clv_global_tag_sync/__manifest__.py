858c8651d8c73afbfeb4d1433bd0c902703874ee
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-11-24 17:54:50 -0300

    [ADD] clv_global_tag_sync

---
+++ clv_global_tag_sync/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Global Tag External Sync',
+    'summary': 'Global Tag External Sync Module used by CLVsol Solutions.',
+    'version': '16.0.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_global_tag',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/global_tag_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
