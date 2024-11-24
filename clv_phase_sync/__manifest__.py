c54fbfc7210a40698096b75b184226f84986a11a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-11-24 17:53:49 -0300

    [ADD] clv_phase_sync

---
+++ clv_phase_sync/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Phase External Sync',
+    'summary': 'Phase External Sync Module used by CLVsol Solutions.',
+    'version': '16.0.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_phase',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/phase_sync.xml',
+        'data/global_settings_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
