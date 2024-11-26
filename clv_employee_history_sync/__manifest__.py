ebe08258695eb1fc8018db2c4763bf44819b5444
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-11-26 19:44:08 -0300

    [ADD] clv_employee_history_sync

---
+++ clv_employee_history_sync/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Employee History External Sync',
+    'summary': 'Employee History External Sync Module used by CLVsol Solutions.',
+    'version': '16.0.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_employee_history',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/hr_employee_history_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
