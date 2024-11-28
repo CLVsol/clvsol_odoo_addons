85bffe7e89e04bd22ed6b41a411ffe925f1e083c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-11-28 20:58:35 -0300

    [ADD] clv_document_sync

---
+++ clv_document_sync/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Document External Sync',
+    'summary': 'Document External Sync Module used by CLVsol Solutions.',
+    'version': '16.0.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_document',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/document_category_sync.xml',
+        'data/document_marker_sync.xml',
+        'data/document_type_1_inc.xml',
+        'data/document_type_2_sync.xml',
+        'data/document_type_parameter_sync.xml',
+        'data/document_1_inc.xml',
+        'data/document_2_inc.xml',
+        'data/document_3_sync.xml',
+        'data/document_item_1_sync.xml',
+        'data/document_item_2_sync.xml',
+        'data/document_item_3_sync.xml',
+        'data/document_item_4_sync.xml',
+        'data/document_item_5_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
