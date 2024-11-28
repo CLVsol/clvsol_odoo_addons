dfe52f6ef1a5b12348b6e289de83fab40897e481
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-11-28 20:57:15 -0300

    [ADD] clv_event_sync

---
+++ clv_event_sync/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Event External Sync',
+    'summary': 'Event External Sync Module used by CLVsol Solutions.',
+    'version': '16.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_event',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/event_sync.xml',
+        'data/event_attendee_1_inc.xml',
+        'data/event_attendee_2_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
