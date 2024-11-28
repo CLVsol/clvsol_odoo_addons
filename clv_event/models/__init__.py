357569ab402a979b0f675eeb0342ba8eba67390d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-11-28 20:56:12 -0300

    [ADD] clv_event

---
+++ clv_event/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import event
+from . import event_category
+from . import global_tag
+from . import event_attendee
+from . import referenceable_model
+from . import phase
+from . import event_state
