a303c3a5ee2234ba22903c1257e976e2f0c6606b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-11 18:23:31 -0300

    [ADD] clv_document_survey_sync

---
+++ clv_document_survey_sync/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Document Survey Association External Sync',
+    'summary': 'Document Survey Association External Sync Module used by CLVsol Solutions.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_document_sync',
+    ],
+    'data': [
+        'data/document_type_1_inc.xml',
+        'data/document_type_2_sync.xml',
+        'data/document_1_inc.xml',
+        'data/document_2_inc.xml',
+        'data/document_3_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
