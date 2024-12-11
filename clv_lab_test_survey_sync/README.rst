1b9d244482baa35413cac2a5d9bf159a9e42d986
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-11 18:24:28 -0300

    [ADD] clv_lab_test_survey_sync

---
+++ clv_lab_test_survey_sync/README.rst
@@ -0,0 +1,11 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=========================================
+Lab Test Survey Association External Sync
+=========================================
+
+Lab Test Survey Association External Sync Module used by CLVsol Solutions.
+
+Developed by `CLVsol <https://github.com/CLVsol>`_.
---
+++ clv_lab_test_survey_sync/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Lab Test Survey Association External Sync',
+    'summary': 'Lab Test Survey Association External Sync Module used by CLVsol Solutions.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_lab_test',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/lab_test_type_1_inc.xml',
+        'data/lab_test_type_2_sync.xml',
+        'data/lab_test_result_1_inc.xml',
+        'data/lab_test_result_2_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
