9279316b9ac5016b7d33202253beac85806886a5
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-11 18:22:25 -0300

    [ADD] clv_patient_history_sync

---
+++ clv_patient_history_sync/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Patient History External Sync',
+    'summary': 'Patient History External Sync Module used by CLVsol Solutions.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_patient_history',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/patient_history_1_inc.xml',
+        'data/patient_history_2_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
