87a69101b0105f5744cc54dc8157dbeaa40a725e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-15 17:20:12 -0300

    [IMP] clv_lab_test_sync

--- clv_lab_test_sync/__manifest__.py
+++ clv_lab_test_sync/__manifest__.py
@@ -18,8 +18,6 @@
         'data/lab_test_type_1_inc.xml',
         'data/lab_test_type_2_sync.xml',
         'data/lab_test_type_parameter_sync.xml',
-        'data/lab_test_request_1_inc.xml',
-        'data/lab_test_request_2_sync.xml',
         'data/lab_test_result_1_inc.xml',
         'data/lab_test_result_2_sync.xml',
         'data/lab_test_criterion_1_sync.xml',
