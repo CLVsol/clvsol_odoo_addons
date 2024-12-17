335e747ae60b401fc7b4544ed1ff4f7545a7833d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-17 14:36:38 -0300

    [ADD] clv_external_sync_jcafb

---
+++ clv_external_sync_jcafb/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'External Sync (customizations for CLVhealth-JCAFB Solution)',
+    'summary': 'External Sync Module customizations for CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_external_sync',
+    ],
+    'data': [
+        # 'data/external_sync.xml',
+        # 'data/external_sync_batch_exec_cron.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
