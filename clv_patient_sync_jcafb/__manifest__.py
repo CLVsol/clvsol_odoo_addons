277a306830ef25f9195e5d1febd996ffe4b6b456
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-17 14:37:08 -0300

    [ADD] clv_patient_sync_jcafb

---
+++ clv_patient_sync_jcafb/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Patient External Sync (for CLVhealth-JCAFB Solution)',
+    'summary': 'Patient External Sync Module used in CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_patient_sync',
+    ],
+    'data': [
+        'data/patient_1_inc.xml',
+        'data/patient_2_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
