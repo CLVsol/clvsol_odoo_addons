61dbcfc0ef5f969d9171e5a64c5f049cb0cabcb4
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-18 15:52:46 -0300

    [ADD] clv_employee_sync_jcafb

---
+++ clv_employee_sync_jcafb/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Employee External Sync (for CLVhealth-JCAFB Solution)',
+    'summary': 'Employee External Sync Module used in CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_employee_sync',
+    ],
+    'data': [
+        'data/employee_2_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
