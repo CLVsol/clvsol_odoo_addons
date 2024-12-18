4ced7871baff533da8599acbc2452e923708bfcc
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-18 15:58:32 -0300

    [ADD] clv_lab_test_sync_jcafb

---
+++ clv_lab_test_sync_jcafb/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Lab Test External Sync (for CLVhealth-JCAFB Solution)',
+    'summary': 'Lab Test External Sync Module used in CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_lab_test_sync',
+    ],
+    'data': [
+        'data/lab_test_type_1_inc.xml',
+        'data/lab_test_type_2_sync.xml',
+        # 'data/lab_test_request_1_inc.xml',
+        # 'data/lab_test_request_2_sync.xml',
+        'data/lab_test_result_1_inc.xml',
+        'data/lab_test_result_2_sync.xml',
+        # 'data/lab_test_report_1_inc.xml',
+        # 'data/lab_test_report_2_sync.xml',
+        'data/lab_test_export_xls_param_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
