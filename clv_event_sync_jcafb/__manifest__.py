a27bc3dff5fa23af8b81ce67c502b0f150123803
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-18 15:56:04 -0300

    [ADD] clv_event_sync_jcafb

---
+++ clv_event_sync_jcafb/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Event External Sync (for CLVhealth-JCAFB Solution)',
+    'summary': 'Event External Sync Module used in CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_event_sync',
+    ],
+    'data': [
+        'data/event_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
