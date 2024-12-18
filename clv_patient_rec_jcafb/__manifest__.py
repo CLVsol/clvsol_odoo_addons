f4bdd89083b3a5b528311be6f3d7064e77cf8289
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-18 17:38:05 -0300

    [ADD] clv_patient_rec_jcafb

---
+++ clv_patient_rec_jcafb/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Patient (Rec) (customizations for CLVhealth-JCAFB Solution)',
+    'summary': 'Patient (Rec) Module customizations for CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_patient_rec',
+        'clv_base_jcafb',
+    ],
+    'data': [
+        'views/patient_rec_code_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
---
+++ clv_patient_rec_jcafb/models/patient_rec_code.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+import re
+
+
+class PatientRec(models.Model):
+    _name = "clv.patient_rec"
+    _inherit = 'clv.patient_rec', 'clv.abstract.code'
+
+    code = fields.Char(string='Patient Code', required=False, default=False)
+    code_sequence = fields.Char(default='clv.person.code')
+
+
+def somaPoderada(numero):
+    i = 0
+    soma = 0
+    while i < len(numero):
+        soma = soma + int(numero[i]) * (15 - i)
+        i = i + 1
+    return soma
+
+
+def validaCNS(numero):
+    numero = str(numero)
+    if numero.isdigit():
+        if re.match(r'[1-2]\d{10}00[0-1]\d$', numero) or re.match(r'[7-9]\d{14}$', numero):
+            return somaPoderada(numero) % 11 == 0
+    return False
+
+    @api.constrains('code')
+    def _check_code(self):
+
+        for record in self:
+
+            if record.code is not False:
+
+                sequence_str = ''
+                for c in record.code:
+                    if c.isdigit():
+                        sequence_str = sequence_str + c
+                sequence_str = sequence_str[:len(sequence_str) - 2]
+                format_code = self.env['clv.abstract.code'].format_code(sequence_str)
+
+                if record.code != format_code:
+                    if not validaCNS(record.code):
+                        raise UserError(u'Invalid Code!')
