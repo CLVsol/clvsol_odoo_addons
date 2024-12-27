0333d9f6da34a265b16e0a1aca9ffc12f38c8917
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-27 16:55:39 -0300

    [ADD] clv_patient_log_jcafb

---
+++ clv_patient_log_jcafb/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Patient Log (for CLVhealth-JCAFB Solution)',
+    'summary': 'Patient Log Module used in CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'images': [],
+    'depends': [
+        'clv_patient',
+        'clv_global_log',
+    ],
+    'data': [
+        'views/patient_log_view.xml',
+        'data/global_log_client.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
