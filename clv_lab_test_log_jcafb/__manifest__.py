1fabcbe66083fc0f086e1163d7e8c5b657069560
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-27 16:55:01 -0300

    [ADD] clv_lab_test_log_jcafb

---
+++ clv_lab_test_log_jcafb/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Lab Test Log (for CLVhealth-JCAFB Solution)',
+    'summary': 'Lab Test Log Module used in CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'images': [],
+    'depends': [
+        'clv_lab_test',
+        'clv_global_log',
+    ],
+    'data': [
+        'views/lab_test_type_log_view.xml',
+        'views/lab_test_result_log_view.xml',
+        'views/lab_test_criterion_log_view.xml',
+        'data/global_log_client.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
