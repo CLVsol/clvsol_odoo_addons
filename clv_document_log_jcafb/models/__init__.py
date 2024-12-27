cb80d1e98416286f5a43dd1b4a9e4ea105198902
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-27 16:54:22 -0300

    [ADD] clv_document_log_jcafb

---
+++ clv_document_log_jcafb/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import document_log
+from . import document_type_log
+from . import document_item_log
