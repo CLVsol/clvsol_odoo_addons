df8a08e888f428c0172a3b5ecee65f858b0c38c4
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-29 20:01:21 -0300

    [ADD] clv_lab_test_export_jcafb

---
+++ clv_lab_test_export_jcafb/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Lab Test Export (for CLVhealth-JCAFB Solution)',
+    'summary': 'Lab Test Export Module used in CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_lab_test_export',
+        'clv_export_jcafb',
+    ],
+    'data': [
+        'views/export_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
