a999d95e476dcac1fa1e3fb3b56119ec82988b1b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2024-12-29 20:00:08 -0300

    [ADD] clv_document_export_jcafb

---
+++ clv_document_export_jcafb/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Document Export (for CLVhealth-JCAFB Solution)',
+    'summary': 'Document Export Module used in CLVhealth-JCAFB Solution.',
+    'version': '16.0.7.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_document_export',
+        'clv_export_jcafb',
+    ],
+    'data': [
+        'views/export_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
