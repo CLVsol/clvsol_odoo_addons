4306e58c60a5d95fedd1a4c8c7469a01bba20154
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2025-02-25 15:40:32 -0300

    [IMP] clv_partner_entity_verification_jcafb (IMP wizard/res_partner_street_pattern_search)

--- clv_partner_entity_verification_jcafb/wizard/res_partner_street_pattern_search.py
+++ clv_partner_entity_verification_jcafb/wizard/res_partner_street_pattern_search.py
@@ -3,7 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
-# from datetime import datetime
 
 from odoo import fields, models
 
@@ -24,45 +23,19 @@ class PartnerStreetPatterntSearch(models.TransientModel):
         default=_default_res_partner_ids
     )
 
-    # def _default_employee_id(self):
-    #     HrEmployee = self.env['hr.employee']
-    #     employee = HrEmployee.search([
-    #         ('user_id', '=', self.env.uid),
-    #     ])
-    #     if employee.id is not False:
-    #         return employee.id
-    #     return False
-    # employee_id = fields.Many2one(
-    #     comodel_name='hr.employee',
-    #     string='Searchd by',
-    #     required=True,
-    #     default=_default_employee_id
-    # )
-
-    # date_searchd = fields.Datetime(
-    #     string='Searchd Date',
-    #     required=True,
-    #     default=lambda *a: datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # )
+    street_pattern_id = fields.Many2one(
+        comodel_name='clv.partner_entity.street_pattern',
+        string='Partner Entity Street Pattern',
+    )
 
     def do_res_partner_street_pattern_search(self):
         self.ensure_one()
 
-        # current_phase_id = int(self.env['ir.config_parameter'].sudo().get_param(
-        #     'clv.global_settings.current_phase_id', '').strip())
+        for res_partner in self.res_partner_ids:
 
-        # for res_partner in self.res_partner_ids:
+            _logger.info(u'%s %s %s', '>>>>>', self.street_pattern_id.street_name, self.street_pattern_id.district)
 
-        #     _logger.info(u'%s %s %s', '>>>>>', res_partner.phase_id.id, res_partner.ref_name)
-
-        #     if (res_partner.phase_id.id == current_phase_id) and \
-        #        (res_partner.state == 'new'):
-
-        #         _logger.info(u'%s %s %s', '>>>>>', self.employee_id.name, self.date_searchd)
-
-        #         res_partner.employee_id_request = self.employee_id
-        #         res_partner.date_searchd = self.date_searchd
-        #         res_partner.state = 'searchd'
-        #         res_partner.reg_state = 'revised'
+            res_partner.street_name = self.street_pattern_id.street_name
+            res_partner.district = self.street_pattern_id.district
 
         return True
