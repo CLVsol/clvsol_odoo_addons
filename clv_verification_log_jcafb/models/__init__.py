233e783251f2d25f856a58ad2a03f5928881c36a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2025-05-04 11:15:51 -0300

    [REM] clv_verification_log_jcafb

--- clv_verification_log_jcafb/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import verification_outcome_log
-from . import verification_template_log
-from . import verification_schedule_log
-from . import verification_batch_log
