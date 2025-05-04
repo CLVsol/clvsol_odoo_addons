04970c28e2dd212e611acd00d48139d024f5c9c7
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2025-05-04 11:11:31 -0300

    [IMP] clv_lab_test_log_jcafb

--- clv_lab_test_log_jcafb/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import lab_test_type_log
-from . import lab_test_result_log
-from . import lab_test_criterion_log
